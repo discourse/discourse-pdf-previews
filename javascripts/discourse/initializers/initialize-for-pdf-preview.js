c0009a15a9f6700f5b11bfef733a2f0c4c54b9f9
Joe <johani@discourse.org>
2020-07-14 04:53:32 +0800

    Add files via upload

---
+++ javascripts/discourse/initializers/initialize-for-pdf-preview.js
@@ -0,0 +1,61 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import Mobile from "discourse/lib/mobile";
+
+export default {
+  name: "my-initializer",
+  initialize() {
+    withPluginApi("0.8.41", api => {
+      if (Mobile.mobileView) return;
+
+      const createPreviewElem = () => {
+        const preview = document.createElement("iframe");
+        preview.height = 450;
+        preview.classList.add("pdf-preview");
+
+        return preview;
+      };
+
+      api.decorateCookedElement(
+        post => {
+          const attachments = [...post.querySelectorAll(".attachment")];
+
+          if (!attachments) return;
+
+          const pdfs = attachments.filter(attachment =>
+            attachment.href.match(/\.[pdf]+$/g)
+          );
+
+          if (!pdfs.length) return;
+
+          pdfs.forEach(pdf => {
+            const preview = createPreviewElem();
+            pdf.append(preview);
+
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", pdf.href, true);
+            xhr.responseType = "blob";
+
+            xhr.onload = function() {
+              if (this.status == 200) {
+                const blob = new Blob([this.response], {
+                  type: "application/pdf"
+                });
+                const url = URL.createObjectURL(blob);
+
+                preview.src = url;
+              }
+            };
+
+            xhr.send();
+            pdf.classList.add("pdf-attachment");
+            pdf.nextSibling.nodeValue = "";
+          });
+        },
+        {
+          id: "pdf-previews",
+          onlyStream: true
+        }
+      );
+    });
+  }
+};
