ae4972e4aa1e5ad21cd752b514c792305987ab01
Joe <xyz.hunabku@gmail.com>
2021-10-20 22:43:42 +0800

    DEV: take #2 refactor for Safari support (#3)
    * revert back to using blob urls and clean up
    
    * src is not available yet
    
    Co-authored-by: Joe <johani@discourse.org>

--- javascripts/discourse/initializers/initialize-for-pdf-preview.js
+++ javascripts/discourse/initializers/initialize-for-pdf-preview.js
@@ -4,32 +4,54 @@ import Mobile from "discourse/lib/mobile";
 
 const PREVIEW_HEIGHT = 500;
 
-const createPreviewElem = () => {
-  const preview = document.createElement("iframe");
-  preview.src = "";
-  preview.height = PREVIEW_HEIGHT;
-  preview.loading = "lazy";
-  preview.classList.add("pdf-preview");
-
-  return preview;
-};
-
 export default {
   name: "pdf-previews",
   initialize() {
     withPluginApi("0.8.41", (api) => {
       if (Mobile.mobileView) return;
 
-      const previewMode = settings.preview_mode;
-      const newTabIcon = () => {
-        const template = document.createElement("template");
-        template.innerHTML = iconHTML("external-link-alt", {
-          class: "new-tab-pdf-icon",
-        });
-        return template.content.firstChild;
-      };
-
       try {
+        const previewMode = settings.preview_mode;
+        const newTabIcon = () => {
+          const template = document.createElement("template");
+          template.innerHTML = iconHTML("external-link-alt", {
+            class: "new-tab-pdf-icon",
+          });
+          return template.content.firstChild;
+        };
+
+        const createElements = () => {
+          const object = document.createElement("object");
+          object.data = "";
+          object.type = "application/pdf";
+          object.height = PREVIEW_HEIGHT;
+          object.loading = "lazy";
+          object.classList.add("pdf-preview");
+
+          const iframe = document.createElement("iframe");
+          iframe.src = "";
+          iframe.type = "application/pdf";
+
+          object.append(iframe);
+
+          return { object, iframe };
+        };
+
+        const setUpPreviewType = (pdf) => {
+          const elements = createElements();
+          if (previewMode === "Inline") {
+            pdf.classList.add("pdf-attachment");
+            pdf.append(elements.object);
+          }
+
+          if (previewMode === "New Tab") {
+            pdf.classList.add("new-tab-pdf");
+            pdf.prepend(newTabIcon());
+          }
+
+          return elements;
+        };
+
         api.decorateCookedElement(
           (post) => {
             const attachments = [...post.querySelectorAll(".attachment")];
@@ -44,6 +66,7 @@ export default {
                 fileSize.nodeValue = "";
               }
 
+              // ignore the pdf if its description starts with white-space
               const startsWithWhitespace = /^\s+/;
               const fileName = pdf.innerText;
 
@@ -52,45 +75,33 @@ export default {
                 return;
               }
 
+              // handle preview type
+              const elements = setUpPreviewType(pdf);
+
+              // the pdf is set to Content-Disposition: attachment; filename="filename.jpg"
+              // one the server. this means we can't just use the href as the
+              // data/src for the pdf preview elements.
               const httpRequest = new XMLHttpRequest();
               httpRequest.open("GET", pdf.href);
-              httpRequest.responseType = "arraybuffer";
+              httpRequest.responseType = "blob";
 
               httpRequest.onreadystatechange = () => {
                 if (httpRequest.readyState !== XMLHttpRequest.DONE) return;
 
                 if (httpRequest.status === 200) {
-                  let src = null;
-                  const blob = new Blob([httpRequest.response], {
-                    type: "application/pdf",
-                  });
+                  const src = URL.createObjectURL(httpRequest.response);
 
-                  // new tab previews
+                  if (previewMode === "Inline") {
+                    elements.object.data = src;
+                    elements.iframe.src = src;
+                  }
+
                   if (previewMode === "New Tab") {
-                    pdf.classList.add("new-tab-pdf");
-                    pdf.prepend(newTabIcon());
-                    src = URL.createObjectURL(blob);
-
                     pdf.addEventListener("click", (event) => {
                       event.preventDefault();
                       window.open(src);
                     });
-
-                    return;
                   }
-
-                  // inline preview
-                  pdf.classList.add("pdf-attachment");
-                  const preview = createPreviewElem();
-                  pdf.append(preview);
-
-                  const reader = new FileReader();
-                  reader.onload = function (event) {
-                    src = event.target.result;
-                    preview.src = src;
-                  };
-
-                  reader.readAsDataURL(blob);
                 }
               };
               httpRequest.send();
